7d0745541dc79c82cd26d3c47b279a72dec7439f
Junphill Hwang <444.gkk@gmail.com>
2022-03-09 16:02:57 +0900

    Refactor : URL slash 제거

--- ably/app/urls.py
+++ ably/app/urls.py
@@ -3,8 +3,8 @@ from .views import UserView, SendPinView, CertifyPhoneView, LoginView, ResetPass
 
 urlpatterns = [
     path('', UserView.as_view()),
-    path('/login', LoginView.as_view()),
-    path('/passwords', ResetPasswordView.as_view()),
-    path('/phones', CertifyPhoneView.as_view()),
-    path('/pins', SendPinView.as_view())
+    path('login', LoginView.as_view()),
+    path('passwords', ResetPasswordView.as_view()),
+    path('phones', CertifyPhoneView.as_view()),
+    path('pins', SendPinView.as_view())
 ]
